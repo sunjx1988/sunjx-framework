448f0d393ebc88021db0ca504cad70794d2b8ada
sunjx <sunjx1988@163.com>
2018-02-25 21:04:47 +0800

    test

---
+++ src/test/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by sunjx on 2018/1/30.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+//            System.out.println("没括号：" + (int) Math.random() * 10);
+            System.out.println("有括号：" + (int) (Math.random() * 10));
+        }
+    }
+}
